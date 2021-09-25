2fc487c2212fea83a363579776f12181411b43ec
braghinDEV <predobraghin2017@gmail.com>
2021-09-25 16:04:40 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>JM| Marmitas Fitness</title>
+        <link rel="stylesheet" href="./css/style.css">
+    </head>
+    <body>
+        <div class="root">
+            <aside class="aside-left"></aside>
+            <aside class="aside-right"></aside>
+            <header class="header">
+                <div class="container menu">
+                    <!--navigation-->
+                    <div class="container-nav">
+                        <nav class="nav">
+                            <div class="container nav-nav">
+                                <ul>
+                                    <li id="home"><a href="#home">HOME</a></li>
+                                    <li><a href="./sub-pages/contact.html">CONTATO</a></li>
+                                    <li><a href="https://www.instagram.com/jm.marmitasfit/" target="_blank">INSTAGRAM</a></li>
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
+                    <!--logo-->
+                    <div class="container-logo">
+                        <div class="container sub-container-logo">
+                            <div class="container-jm">JM</div>
+                            <h2> Marmitas Fitness </h2>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <!---presentation-->
+            <div class="presentation">
+                <section class="section-presentation">
+                    <div class="container">
+                        <h2>Oferecemos saúde através da alimentação!</h2>
+                        <div class="container btn-presentation">
+                            <div class="container">
+                                <div class="container main-title">
+                                    <h1>JM<span class="jm">Marmitas Fitness</span></h1>
+                                </div>
+                            </div>
+                            <div class="container">
+                                <h2>Oferecemos saúde através da alimentação</h2>
+                                <button class="btn"><a href="#combos">COMBOS</a></button>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <!---footer-->
+            <footer class="footer">
+                <span class="arr">
+                    © 2021  JM Marmitas Fitness - Todos os direitos reservados
+                </span>
+            </footer>
+        </div>
+    </body>
+</html>
